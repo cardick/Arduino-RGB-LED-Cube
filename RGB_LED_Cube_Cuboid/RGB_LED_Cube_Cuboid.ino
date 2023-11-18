08f9d953fee16e089651573b17f5c13545c2102f
Carsten <102875776+cardick@users.noreply.github.com>
2023-11-18 13:53:30 +0100

    getting it work
    adjustments and bugfixes, so that it works with the current hardware

--- RGB_LED_Cube_Cuboid/RGB_LED_Cube_Cuboid.ino
+++ RGB_LED_Cube_Cuboid/RGB_LED_Cube_Cuboid.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 /**
  * Controll a common anode RGB LED cube or cuboid - inspired by the projects of 
- * Kevin Darrah and Tiemen Waterreus.
+ * Kevin Darrah and Tiemen Waterreus. 
  *
  * The driver methods use SPI to control the shift registers. 
  *
@@ -14,7 +14,7 @@ const uint8_t clockPin = 13;
 // must be 11 defined by SPI
 const uint8_t dataPin = 11;
 
-const uint8_t latchPin = 10;
+const uint8_t latchPin = 2;
 // @todo: Find out for whatever reason Kevin has used this blank pin
 const uint8_t blankPin = 4;
 
@@ -42,6 +42,10 @@ const uint8_t LED_LAYERS = 2;
 // 4 bit - bit angle modulation (BAM)
 const uint8_t BAM = 4;
 
+// maximum and minimum brightness for LEDs
+const uint8_t MAXBRIGHT = 15;
+const uint8_t MINBRIGHT = 0;
+
 // bit mask for the led state information (LED_COLUMS * 3, because each LED has three
 // cathode pins).
 bool ledCubeBitMask[BAM][LED_ROWS][LED_COLUMNS * 3][LED_LAYERS];
@@ -51,87 +55,114 @@ uint8_t currentTick = 0;
 uint8_t currentLayer = 0;
 
 void setup() {
-  // disable interrupts
-  cli();  
 
-  SPI.begin();
+  //SPI.begin();
   SPI.setBitOrder(MSBFIRST);  // most significant bit first vs. least significant bit first
   // Set LSB as bit order, this means we will send the least significant bit first and it
   // will be written to Q7 = Register Pin 7 with the most significant bit being written last
   // to Q0 or Pin 15
+  
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+
+  // disable interrupts
+  //cli();
+  noInterrupts();
 
   // initialize the timer interrupts to enable multiplexing
 
-  // set TCCR1A register to 0
-  TCCR1A = 0;
-  // set TCCR1B register to 0    
-  TCCR1B = 0;
-  // reset counter value
-  TCNT1 = 0;
+  // set TCCR1A register 
+  TCCR1A = B00000000;
+  // set TCCR1B register 
+  TCCR1B = B00001011;
 
-  // triggers the time of the interrupt - justify, by modifying this value
-  OCR1A = 30;
+  // triggers the time of the interrupt
+  // an interval faster than 70 seems to mix the colors between layers
+  OCR1A = 70;
 
-  // set prescaler
-  TCCR1B |= (1 << CS12) | (1 << CS10);
-
-  // turn on CTC mode
-  TCCR1B |= (1 << WGM12);
-  // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
+  TIMSK1 = B00000010;
 
   // Configure the Arduino pins used as OUTPUT, so they can send data to the Shift Register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  pinMode(blankPin, OUTPUT);
 
+  SPI.begin();
+  
   // clear all leds
   clearLeds();
 
   // allow interrupts
-  sei();
+  //sei();
+  interrupts();
 
   // start serial connection - for debugging reasons
   // Serial.begin(9600);
-  delay(200);
 }
 
 /**
 * Within the loop only the bytes should be manipulated that are written out in ISR method
 */
 void loop() {
+    // functional check
+    setLed(0, 0, 0, MAXBRIGHT, MINBRIGHT, MINBRIGHT);
+    setLed(0, 0, 1, MINBRIGHT, MINBRIGHT, MAXBRIGHT);
+    delay(500);
+    setLed(0, 0, 0, MINBRIGHT, MAXBRIGHT, MINBRIGHT);
+    setLed(0, 0, 1, MAXBRIGHT, MINBRIGHT, MINBRIGHT);
+    delay(500);
+    setLed(0, 0, 0, MINBRIGHT, MINBRIGHT, MAXBRIGHT);
+    setLed(0, 0, 1, MINBRIGHT, MAXBRIGHT, MINBRIGHT);
+    delay(500);
+    setLed(0, 0, 0, MAXBRIGHT, MAXBRIGHT, MAXBRIGHT);
+    setLed(0, 0, 1, MAXBRIGHT, MAXBRIGHT, MAXBRIGHT);
+    delay(1000);
+    setLed(0, 0, 0, MINBRIGHT, MINBRIGHT, MINBRIGHT);
+    setLed(0, 0, 1, MINBRIGHT, MINBRIGHT, MINBRIGHT);
+    delay(1000);
+
+    fadingTest();
+}
+
+void fadingTest(){
   // fade in
   for (int i = 0; i < 12; i++) {
     setLed(0, 0, 0, i, i, 0);
     setLed(0, 0, 1, 0, i, i / 2);
-    delay(10);
+    delay(120);
   }
 
-  delay(200);
+  delay(1000);
 
   // fade bottom one from red to blue
   for (int i = 0; i < 12; i++) {
     setLed(0, 0, 0, 12 - i, 12, i);
-    delay(10);
+    delay(120);
   }
 
-  delay(200);
+  delay(1000);
 
   // fade color from upper green to lower red
   for (int i = 0; i < 12; i++) {
     setLed(0, 0, 0, 0, 12 - i, 12);
     setLed(0, 0, 1, i, 12, 6);
-    delay(10);
+    delay(120);
   }
 
-  delay(200);
+  delay(1000);
 
   // fade all out, and see if it crashes
   for (int i = 0; i < 12; i++) {
     setLed(0, 0, 0, 0, 0, 12 - i);
     setLed(0, 0, 1, 12 - i, 12 - i, 6 - (i / 2));
-    delay(10);
+    delay(120);
   }
+
+  // set off
+  setLed(0, 0, 0, 0, 0, 0);
+  setLed(0, 0, 1, 0, 0, 0);
+  delay(1000);
 }
 
 /**
@@ -211,6 +242,9 @@ ISR(TIMER1_COMPA_vect) {
   // there is maybe an offset of unused ports on the shift registers; this depends on the cubes size
   int shift = (((LED_COLUMNS * 3 * LED_ROWS) + LED_LAYERS) % 8) - 1;
 
+// Serial.print("tick: ");
+// Serial.println(currentTick);
+
   // The idea is to shift out the led information from back to front from the perspective of the
   // daisy chained shift registers. Shift out only the current layer for the current tick given by
   // the BAM duty cycle. The rest is done by Arduino by repetetive calls of this method.
@@ -233,12 +267,17 @@ ISR(TIMER1_COMPA_vect) {
   // connected in first position (MSBFIRST)
   for (int i = LED_LAYERS - 1; i >= 0; i--) {
     if (i == currentLayer) {
-      transferByte = transferByte | (1 << shift--);
-    } else {
-      transferByte = transferByte | (0 << shift--);
+      transferByte = transferByte | (1 << shift);
     }
+    shift--;
+    // else {
+    //   transferByte = transferByte | (0 << shift--);
+    // }
 
     if (shift < 0) {
+      // Serial.print("transfer: ");
+      // Serial.println(transferByte, BIN);
+
       //shift out the byte
       SPI.transfer(transferByte);
 
@@ -249,12 +288,17 @@ ISR(TIMER1_COMPA_vect) {
   }
 
   // set latch low than high to activate shift registers
-  PORTD &= 0 << latchPin;
+  // PORTD &= 0 << latchPin;
+  // PORTD |= 1 << latchPin;
   PORTD |= 1 << latchPin;
+  PORTD &= ~(1 << latchPin);
+  PORTD &= ~(1 << blankPin);
+
+  //Serial.println("latch impuls");
 
   // reset current layer to the first one, when all layers have been shifted out
   currentLayer = ((currentLayer < LED_LAYERS - 1) ? (currentLayer + 1) : 0);
-
+  
   // when all layers for this tick were shifted out repeat for next tick
   if (currentLayer == 0) {
     // prepare for next tick or restart for next duty cycle
